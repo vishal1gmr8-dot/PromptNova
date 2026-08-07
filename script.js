5c067f7aaea5747822cd4f56245fb4b655e50847
vishal1gmr8-dot <vishal1gmr8@gmail.com>
2026-08-07 12:46:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ function copyPrompt(id) {
     navigator.clipboard.writeText(text);
     alert("✅ Prompt Copied!");
 }
+const search = document.getElementById("search");
+
+if (search) {
+  search.addEventListener("keyup", function () {
+    const value = this.value.toLowerCase();
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+      const text = card.innerText.toLowerCase();
+
+      if (text.includes(value)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
+  });
+}
